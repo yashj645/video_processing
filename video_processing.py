f69b0163cfd50cdbb9802213b30aca6d6c704a26
yashj645 <yashj645@gamil.com>
2018-08-25 13:48:40 +0530

    first commit

---
+++ video_processing.py
@@ -0,0 +1,17 @@
+import cv2,time
+
+
+video=cv2.VideoCapture(0)
+a=0
+while True:
+    a=a+1
+    check, frame=video.read()
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("capturing",gray)
+    key=cv2.waitKey(1)
+    if key==ord('q'):
+        break;
+print(a)
+video.release()
+cv2.destroyAllWindows()
